aa7a0da2232b1d0f5e6a920cebb58dd13a382305
kevin523523 <89325741+kevin523523@users.noreply.github.com>
2025-06-16 21:20:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.set('trust proxy', 1);
 // 2) CORS - Configuración específica para CAS
 // --------------------------------------------------
 app.use(cors({
-  origin: *,
+  origin: "*",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
