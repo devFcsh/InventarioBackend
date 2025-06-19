26aa7177dbc127d1324134cb678ee5152fc6d369
kevin523523 <89325741+kevin523523@users.noreply.github.com>
2025-06-19 07:21:00 -0500

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,12 +1,14 @@
-import { Sequelize, DataTypes } from "sequelize";
+import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const { DB_DATABASE, DB_USERNAME, DB_PASSWORD } = process.env;
+const { DB_DATABASE, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
 
 const db = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
-  dialect: "mysql",
+  host: DB_HOST,
+  port: DB_PORT,
+  dialect: "mysql"
 });
 
 export default db;
