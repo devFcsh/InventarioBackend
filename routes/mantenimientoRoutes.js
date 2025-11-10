f9d2dc6da05bc537f970756e66252e81ec821516
jairrami06 <jairrami@espol.edu.ec>
2025-11-10 11:30:41 -0500

    feat: changes in mantenimientos

--- routes/mantenimientoRoutes.js
+++ routes/mantenimientoRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { agregarMantenimiento, obtenerActividadesEquipo, obtenerMantenimientos } from "../controllers/mantenimientoController.js";
+import { agregarMantenimiento, editarMantenimiento, eliminarMantenimiento, obtenerActividadesEquipo, obtenerMantenimientos } from "../controllers/mantenimientoController.js";
 const router = Router();
 
 router.get("/actividades/:id", obtenerActividadesEquipo);
 router.get("/:id", obtenerMantenimientos);
 router.post("/", agregarMantenimiento);
+router.put("/:id", editarMantenimiento);
+router.delete("/:id", eliminarMantenimiento);
 
 export default router;
